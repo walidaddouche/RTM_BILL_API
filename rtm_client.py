0f0d21983e92958686687d6ae3034c1863b98cb7
ADDOUCHE Walid <73178026+walidaddouche@users.noreply.github.com>
2022-12-24 14:17:09 +0100

    Update

--- rtm_client.py
+++ rtm_client.py
@@ -1,19 +1,35 @@
 import os
 import requests
-from dotenv import load_dotenv
+
+__all__ = ['RtmClient']
+
+
+def load_env_variables():
+    with open(".ENV") as f:
+        for line in f:
+            if line[0] != '#' and '=' in line:
+                key, value = line.split('=', 1)
+                key = key.strip()
+                value = value.strip()
+                os.environ[key] = value
+
+
+# Chargez les variables d'environnement à partir du fichier .env
 
 
 class RtmClient:
+
     def __init__(self):
         # Load environment variables from .env file
         self.token = None
         self.code_client = None
         self.user_id = None
-        load_dotenv()
 
         # Get email and password from environment variables
-        self.EMAIL_USER = os.getenv('EMAIL_RTM')
-        self.PASSWORD_USER = os.getenv('PASSWORD_RTM')
+
+        load_env_variables()
+        self.EMAIL_USER = os.environ.get('EMAIL_RTM')
+        self.PASSWORD_USER = os.environ.get('PASSWORD_RTM')
 
         # API endpoints
         self.api_url = "https://api.rtm.fr"
@@ -66,11 +82,10 @@ class RtmClient:
         list_vente_data = list_vente_response.json()
 
         # parse the Json response
-        bills = list_vente_data["data"]["list"]
-        print(len(bills))
-        return bills
 
-    def get_bill_By_id(self, id_bill):
+        return list_vente_data["data"]["list"]
+
+    def get_bill_By_Id(self, id_bill):
         bill_url = f"https://api.rtm.fr/MaRtm/printPDF/attestations?ids={id_bill}" \
                    f"&userId={self.user_id}&token={self.token}"
         return bill_url
@@ -82,8 +97,13 @@ if __name__ == "__main__":
     if rtm_client.login():
         # Get list of bills
         bills = rtm_client.get_all_bills()
+        if len(bills) > 0:
+            for bill in bills:
+                bill_id = bill["PK_VENTES"]
+                url = rtm_client.get_bill_By_Id(bill_id)
+                print("The URL for bill with ID {} is: {}".format(bill_id, url))
+        else:
+            print("The bill list is empty.")
 
-        for bill in bills:
-            print(rtm_client.get_bill_By_id(bill["PK_VENTES"]))
-    else:
-        print("Error: Login failed")
+else:
+    print("Error: Login failed")
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='rtm_client',  # Nom de votre librairie
+    version='0.1',  # Numéro de version de votre librairie
+    py_modules=['rtm_client'],  # Liste des modules à inclure dans la librairie
+    author='Walid',  # Nom de l'auteur de la librairie
+    author_email='##',  # Adresse email de l'auteur
+    url='https://github.com/walidaddouche/RTM_BILL_MAILER',  # URL du référentiel sur GitHub
+    license='MIT',  # Licence sous laquelle votre librairie est publiée
+    description='A RTM client library',  # Description de votre librairie
+    install_requires=['requests', "dotenv"],  # Liste des dépendances de votre librairie
+)
