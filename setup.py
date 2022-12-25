662be3f2f85aacc455c8cc1ffb5758702fadb35c
ADDOUCHE Walid <walid.add93@gmail.com>
2022-12-26 00:28:40 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     py_modules=['rtm_client'],  # Liste des modules à inclure dans la librairie
     author='Walid',  # Nom de l'auteur de la librairie
     author_email='##',  # Adresse email de l'auteur
-    url='https://github.com/walidaddouche/RTM_BILL_MAILER',  # URL du référentiel sur GitHub
+    url='https://github.com/walidaddouche/RTM_BILL_API',  # URL du référentiel sur GitHub
     license='MIT',  # Licence sous laquelle votre librairie est publiée
-    description='A RTM Bill mailer',  # Description de votre librairie
-    install_requires=['requests', 'rtm-client'],  # Liste des dépendances de votre librairie
+    description='A RTM Bill API',  # Description de votre librairie
+    install_requires=['requests'],  # Liste des dépendances de votre librairie
 )
